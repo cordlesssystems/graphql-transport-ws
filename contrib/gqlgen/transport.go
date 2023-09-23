2799ba0b197f95efe139a048f9b27c93948e8f4a
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2023-09-23 17:18:51 +0300

    contrib gqlgen mvp

--- contrib/gqlgen/transport.go
+++ contrib/gqlgen/transport.go
@@ -1,11 +1,17 @@
 package gqlgen
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"github.com/99designs/gqlgen/graphql"
+	graphql_transport_ws "github.com/cordlesssystems/graphql-transport-ws"
+	"io"
 	"net/http"
 )
 
 type Transport struct {
+	handleInit graphql_transport_ws.HandleInitFunc
 }
 
 func (t *Transport) Supports(r *http.Request) bool {
@@ -13,6 +19,79 @@ func (t *Transport) Supports(r *http.Request) bool {
 }
 
 func (t *Transport) Do(w http.ResponseWriter, r *http.Request, exec graphql.GraphExecutor) {
-	//TODO implement me
-	panic("implement me")
+
+	var handleInit graphql_transport_ws.HandleInitFunc = func(ctx context.Context, payload graphql_transport_ws.InitPayload) (context.Context, graphql_transport_ws.InitAckPayload, error) {
+		return ctx, graphql_transport_ws.InitAckPayload{}, nil
+	}
+
+	if t.handleInit != nil {
+		t.handleInit = handleInit
+	}
+
+	h := graphql_transport_ws.NewHandler(
+		t.handleInit,
+		func(ctx context.Context, operationId string, payload json.RawMessage) (<-chan graphql_transport_ws.ExecutionResult, <-chan error, error) {
+
+			ctx = graphql.StartOperationTrace(ctx)
+			var params *graphql.RawParams
+			if err := jsonDecode(bytes.NewReader(payload), &params); err != nil {
+				return nil, nil, err
+			}
+
+			start := graphql.Now()
+			params.ReadTime = graphql.TraceTiming{
+				Start: start,
+				End:   graphql.Now(),
+			}
+
+			rc, err := exec.CreateOperationContext(ctx, params)
+			if err != nil {
+				resp := exec.DispatchError(graphql.WithOperationContext(ctx, rc), err)
+				return nil, nil, resp.Errors
+			}
+
+			exResCh := make(chan graphql_transport_ws.ExecutionResult)
+			errCh := make(chan error)
+
+			go func() {
+				defer close(exResCh)
+				defer close(errCh)
+
+				var (
+					responses  graphql.ResponseHandler
+					execResult json.RawMessage
+					serErr     error
+				)
+				responses, ctx = exec.DispatchOperation(ctx, rc)
+				for {
+					response := responses(ctx)
+					if response == nil {
+						break
+					}
+
+					execResult, serErr = json.Marshal(response)
+
+					if serErr != nil {
+						errCh <- serErr
+						return
+					}
+
+					exResCh <- graphql_transport_ws.ExecutionResult(execResult)
+
+				}
+
+			}()
+
+			return exResCh, errCh, nil
+		},
+	)
+
+	h.ServeHTTP(w, r)
+
+}
+
+func jsonDecode(r io.Reader, val interface{}) error {
+	dec := json.NewDecoder(r)
+	dec.UseNumber()
+	return dec.Decode(val)
 }
